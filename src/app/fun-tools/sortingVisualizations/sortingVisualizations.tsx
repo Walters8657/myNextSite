d86ab6458c20767e766f103a7003942886514a9c
Michael Walters <waltersm305@gmail.com>
2025-08-09 15:24:15 -0400

    Adds selection, insertion, and merge sort

--- src/app/fun-tools/sortingVisualizations/sortingVisualizations.tsx
+++ src/app/fun-tools/sortingVisualizations/sortingVisualizations.tsx
@@ -15,8 +15,22 @@ export default function SortingVisualizations() {
         setBars(Array.from({ length: numBars }, (_i, j) => ({ order: j })));
     }
 
-    const shuffleBars = () => {
-        setBars([...bars].sort(() => Math.random() - 0.5));
+    const shuffleBars = async () => {
+        let newBars = [...bars];
+        for (let i = newBars.length - 1; i >= 0; i--) {
+            let rand = Math.floor(Math.random() * (i + 1));
+            newBars = swapBars(newBars, i, rand);
+            setBars([...newBars]);
+            await new Promise(resolve => setTimeout(resolve, 0));
+        }
+    }
+
+    const swapBars = ( array: Bar[], i: number, j: number) => {
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+        return array;
     }
 
     const bubbleSort = async () => {
@@ -26,27 +40,80 @@ export default function SortingVisualizations() {
         while (swapped) {
             swapped = false;
             for (let i = 0; i < newBars.length - 1; i++) {
-                // If out of order, swap the bars
+                // If out of order, swap the bars and update state
                 if (newBars[i].order > newBars[i + 1].order) {
-                    // Swap the bars
-                    let temp = newBars[i];
-                    newBars[i] = newBars[i + 1];
-                    newBars[i + 1] = temp;
+                    newBars = swapBars(newBars, i, i + 1);
                     swapped = true;
-                    
-                    // Update state with current step
                     setBars([...newBars]);
-                    
-                    // Wait before next step
                     await new Promise(resolve => setTimeout(resolve, 0));
                 }
             }
         }
     }
 
-    const selectionSort = async () => { alert("Selection Sort"); }
-    const insertionSort = async () => { alert("Insertion Sort"); }
-    const mergeSort = async () => { alert("Merge Sort"); }
+    const selectionSort = async () => { 
+        let newBars = [...bars];
+        for (let i = 0; i < newBars.length - 1; i++) {
+            for (let n = i + 1; n < newBars.length; n++) {
+                if (newBars[i].order > newBars[n].order) {
+                    newBars = swapBars(newBars, i, n);
+                    setBars([...newBars]);
+                    await new Promise(resolve => setTimeout(resolve, 0));
+                }
+            }
+        }
+     }
+
+    const insertionSort = async () => {
+        let newBars = [...bars];
+        for (let i = 1; i < newBars.length; i++) {
+            let j = i;
+            // Bubble sort downwards to keep the bars in order
+            while (j > 0 && newBars[j - 1].order > newBars[j].order) {
+                newBars = swapBars(newBars, j - 1, j);
+                setBars([...newBars]);
+                await new Promise(resolve => setTimeout(resolve, 0));
+                j--;
+            }
+        }
+    }
+
+    const mergeSort = async () => {
+        let newBars = [...bars];
+        const n = newBars.length;
+        for (let bucketSize = 1; bucketSize < n; bucketSize *= 2) {
+            // Iterate through the bars in chunks of size bucketSize
+            for (let left = 0; left < n; left += 2 * bucketSize) {
+                let mid = Math.min(left + bucketSize, n);
+                const right = Math.min(left + 2 * bucketSize, n);
+                if (mid >= right) continue;
+
+                let i = left;
+                let j = mid;
+
+                // Iterate through the bars in the chunk
+                while (i < j && j < right) {
+                    // If the bars are in order, move the pointers forward
+                    if (newBars[i].order <= newBars[j].order) {
+                        i++;
+                    } else {
+                        // Bubble sort to keep the bars in order
+                        for (let k = j; k > i; k--) {
+                            newBars = swapBars(newBars, k, k - 1);
+                        }
+                        // Update the bars UI
+                        setBars([...newBars]);
+                        await new Promise(resolve => setTimeout(resolve, 30));
+                        i++;
+                        j++;
+                        mid++;
+                    }
+                }
+            }
+        }
+        setBars([...newBars]);
+    }
+
     const quickSort = async () => { alert("Quick Sort"); }
 
     useEffect(() => {
