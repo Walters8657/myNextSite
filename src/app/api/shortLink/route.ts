211172a26d22dd1232dfdbf59fd790eb260eb1d6
Michael Walters <waltersm305@gmail.com>
2026-01-27 20:15:53 -0500

    api route cleanup

--- src/app/api/shortLink/route.ts
+++ src/app/api/shortLink/route.ts
@@ -1,13 +1,15 @@
 import { NextResponse, NextRequest } from "next/server";
 import { drizzle } from 'drizzle-orm/node-mssql';
-import { shortLinkTable } from "../../../db/schema"
+import { shortLinkTable } from "../../../db/schema";
 import '../../../../envConfig';
 
 const db = drizzle(process.env.DATABASE_STRING!);
 
 // To handle a GET request to /api
 export async function GET(request: any) {
-  let shortLinks = await db.select().from(shortLinkTable);
+  let shortLinks = await db
+    .select()
+    .from(shortLinkTable);
   return NextResponse.json({ shortLinks: shortLinks }, { status: 200 });
 }
 
@@ -16,14 +18,17 @@ export async function POST(request: Request) {
   const data = await request.json();
 
   if (data.slug && data.longLink) {
-    const result = await db.insert(shortLinkTable).values({
-      slug: data.slug,
-      longLink: data.longLink
-    });
+    try {
+      const result = await db.insert(shortLinkTable).values({
+        slug: data.slug,
+        longLink: data.longLink
+      });
+    } catch (sqlError: any) {
+      return NextResponse.json({message: sqlError.message}, {status: 500});
+    }
 
     return NextResponse.json({ message: "Values Inserted" }, { status: 200 });
   } else {
-    return NextResponse.json({ message: "Invalid values" }, { status: 200 });
+    return NextResponse.json({ message: "Expected JSON keys not found." }, { status: 400 });
   }
-
 }
